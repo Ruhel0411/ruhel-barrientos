ca1da895746ae92bd734dd56bfff81f1ec70eac3
ruhe0411 <202213411a@utea.edu.pe>
2024-01-16 13:07:07 -0500

    ejercicio02

---
+++ Ejercicios01/ejercicio03/ejercicio03.java
@@ -0,0 +1,21 @@
+package Ejercicios01.ejercicio03;
+
+public class ejercicio03 {
+    public class Ejercicio3_1 {
+        public static void main(String[] args) {
+          System.out.println("  Español      Inglés");
+          System.out.println("-----------------------------------------");
+          System.out.printf("%-10s      %-10s\n",  "computer",  "computadora");
+          System.out.printf("%-10s      %-10s\n",  "student",  "estudiante");
+          System.out.printf("%-10s      %-10s\n",  "cat", "gato");
+          System.out.printf("%-10s      %-10s\n",  "penguin", "pinguino");
+          System.out.printf("%-10s      %-10s\n",  "machine", "maquina");
+          System.out.printf("%-10s      %-10s\n",  "nature", "natural");
+          System.out.printf("%-10s      %-10s\n",  "linght", "luz");
+          System.out.printf("%-10s      %-10s\n",  "greet", "verde");
+          System.out.printf("%-10s      %-10s\n",  "book", "libro");
+          System.out.printf("%-10s      %-10s\n",  "pyramid", "piramide");
+          
+        }        
+}
+}
---
+++ Ejercicios01/ejercicios02.java/ejercicios02.java
@@ -0,0 +1,13 @@
+package.ejercicios02
+
+ejercicios02.java;
+
+public class ejercicios02 {
+    public class Ejercicio02 { // Clase principal
+        public static void main(String[] args) {
+          System.out.println("RUHEL BARRIENTOS");
+          System.out.println("Avenida virgen del rosario,tamburco");
+          System.out.println("954325634");
+        }
+    }
+}
